34ceae1d19b1aea7c04f7357f1a22de39ff385c2
Natalka Pashchuk <fargai@ukr.net>
2023-03-28 21:56:50 +0300

    Add 01-gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const ulList = document.querySelector('.gallery');
+
+const listGallery = galleryItems.map(item => 
+`
+<li class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</li>
+`).join('');
+
+ulList.insertAdjacentHTML('beforeend', listGallery);
+ulList.addEventListener('click', (event) => {
+    event.preventDefault();
+    const clickEl = event.target;
+    const srcImg = clickEl.dataset.source;
+
+    const instance = basicLightbox.create(`<img src="${srcImg}">`);
+
+    instance.show();
+});
+
+
+
+
+
+
+
+
+
+
 console.log(galleryItems);
